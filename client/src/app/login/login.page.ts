e28bb07effe3ac004a116f9e3ee555714cfcca2b
Jihyoung <59171154+HelloJihyoung@users.noreply.github.com>
2021-04-21 19:22:34 +0900

    login added

--- client/src/app/login/login.page.ts
+++ client/src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import axios from 'axios';
 
 @Component({
   selector: 'app-login',
@@ -7,16 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  user:String;
-  pass:String;
+  user: String;
+  pass: String;
 
-  constructor() { }
+  constructor(private router: Router) { }
+  username: string;
+  password: string;
 
   ngOnInit() {
+
   }
 
-  signIn() {
-    console.log('Username: ', this.user, 'Password: ', this.pass)
+  login() {
+    axios.post('http://localhost:3000/login', {
+      username: this.username,
+      password: this.password
+    })
+      .then((res) => {
+        console.log(res.data);
+        if (res.data == "로그인 성공") {
+          alert("로그인 되었습니다");
+          this.router.navigate(['/home']);
+        }else if (res.data == "아이디와 비밀번호 불일치"){
+          alert("비밀번호가 틀렸습니다.");
+          this.router.navigate(['/login']);
+        }
+        else if (res.data == "존재하지 않는 아이디"){
+          alert("계정 정보가 없습니다. 회원가입 해주세요");
+          this.router.navigate(['/join']);
+        }
+        console.log(res);
+      })
   }
-
 }
