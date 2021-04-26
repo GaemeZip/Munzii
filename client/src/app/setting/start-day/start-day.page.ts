0db618fd8b92871f0cdcf7e940331c1484115f5d
Jihyoung <59171154+HelloJihyoung@users.noreply.github.com>
2021-04-26 19:34:05 +0900

    add start day

--- client/src/app/setting/start-day/start-day.page.ts
+++ client/src/app/setting/start-day/start-day.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import axios from 'axios';
 
 @Component({
   selector: 'app-start-day',
@@ -8,11 +9,73 @@ import { Router } from '@angular/router';
 })
 export class StartDayPage implements OnInit {
   windowHeight: number = window.screen.height;
-  
+  day:{
+    s_id: number,
+    s_day: string
+  } = {
+    s_id: 0,
+    s_day: ""
+  }
+  dayList: any[] =[];
+  selectedDay: any;
+
   constructor(private router:Router) { 
   }
 
   ngOnInit() {
+    axios.get('http://localhost:3000/readStartDay')
+      .then(res => {        
+        for (var i = 0; i < res.data.length ; i++) {
+          this.day.s_id = res.data[i].s_id;
+          this.day.s_day = res.data[i].s_day;
+          this.dayList.push([this.day.s_id, this.day.s_day]);
+        }
+      });
+      this.initDay();
+  }
+  initDay(){
+    axios.get('http://localhost:3000/currentStartDay')
+      .then(res => {        
+        //console.log("받아온 font id 값 : "+res.data[0].font_id);
+         this.selectedDay = res.data[0].start_day_id;
+         this.selectIcon(this.selectedDay);
+      });
+  }
+  selectIcon(id){
+    console.log(id+"가 선택되었습니다")
+    for (let index = 1; index <= this.dayList.length; index++) {
+      var numToString = index.toString();
+      var elementSelected = document.getElementById(numToString);
+      elementSelected.classList.remove("selected");
+    }
+    var elementSelected = document.getElementById(id);
+    elementSelected.classList.add("selected");
+    const changeFont = document.querySelector('body');
+    changeFont.style.setProperty('--ion-font-family', this.dayList[id-1][1]);
+  }
+
+  updateStartDay(s_id) {
+    axios.post('http://localhost:3000/updateStartDay', {
+      start_day_id: s_id,
+      u_id: 1
+    }).then((res) => {
+      console.log(res)
+      if (res.data != 'error') {
+        console.log("폰트 업데이트");
+        console.log(res)
+        console.log(s_id+this.dayList[s_id-1][1]);
+      } else {
+        console.log("에러 발생")
+      }
+    })
+  }
+
+  select(id){
+    console.log("클릭되었습니다 : " + id);
+    this.updateStartDay(id); // update db
+    this.selectIcon(id); // display munzii
+    this.selectedDay = id;
+    //this.initFont();
   }
 
   prev(){
---
+++ server/router/settings/currentStartDay.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mysql = require('mysql');
+const router = express.Router();
+const dbconfig = require('../db.js');
+const connection = mysql.createConnection(dbconfig);
+
+router.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+router.get('/', (req, res) => {
+    //var start_day_id = req.body.start_day_id;
+    var u_id = req.body.u_id;
+
+    //임의로 1이라고 넣어둠!
+    var selectQuery = 'SELECT start_day_id FROM users WHERE u_id=1';
+    var params = [u_id];
+
+    connection.query(selectQuery, params, (error, rows, data) => {
+        if (error) {
+            res.send('error');
+        }
+        else {
+            //id값 보내주기
+            res.send(rows);
+        }
+        console.log(rows);
+    });
+});
+
+module.exports = router;
+
