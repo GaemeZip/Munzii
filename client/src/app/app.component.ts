492fd6ad6519968dde351735fbdfefa2f46f1aea
Jihyoung <59171154+HelloJihyoung@users.noreply.github.com>
2021-05-03 01:42:28 +0900

    replace

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -10,18 +10,27 @@ export class AppComponent {
   ngOnInit() {
     const change = document.querySelector('body');
 
-    if (localStorage.f_family == null) { // default
+    if (localStorage.themeId == null) { // default
       console.log("없음!")
-      change.style.setProperty('--ion-font-family', 'NexonGothic');
+      localStorage.f_family = 'NexonGothic';
+      localStorage.themeId = 1;
+      localStorage.t_primary = '#FFF3A9';
+      localStorage.t_check = '#97C1E87E';
+      localStorage.t_complete = '#FDE2E2';
+      localStorage.t_button ='#F9D667';
+      localStorage.t_darkYellow = '#FDE2E2';
+      localStorage.t_background = '#FCECB6';
 
-      change.style.setProperty('--ion-color-primary', '#FFF3A9');
-      change.style.setProperty('--ion-color-check', '#97C1E87E');
-      change.style.setProperty('--ion-color-complete', '#FDE2E2');
-      change.style.setProperty('--ion-color-button', '#F9D667');
-      change.style.setProperty('--ion-color-dark-yellow', '#FDE2E2');
-      change.style.setProperty('--ion-theme-background', '#FCECB6');
+      // change.style.setProperty('--ion-font-family', 'NexonGothic');
+
+      // change.style.setProperty('--ion-color-primary', '#FFF3A9');
+      // change.style.setProperty('--ion-color-check', '#97C1E87E');
+      // change.style.setProperty('--ion-color-complete', '#FDE2E2');
+      // change.style.setProperty('--ion-color-button', '#F9D667');
+      // change.style.setProperty('--ion-color-dark-yellow', '#FDE2E2');
+      // change.style.setProperty('--ion-theme-background', '#FCECB6');
     }
-    
+
     change.style.setProperty('--ion-font-family', localStorage.f_family);
 
     change.style.setProperty('--ion-color-primary', localStorage.t_primary);
