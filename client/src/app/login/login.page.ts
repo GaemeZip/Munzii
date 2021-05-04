b368f4911ff774319713dc6eb0f28053500cc069
Jihyoung <59171154+HelloJihyoung@users.noreply.github.com>
2021-05-04 14:20:57 +0900

    handle error

--- client/src/app/login/login.page.ts
+++ client/src/app/login/login.page.ts
@@ -17,8 +17,7 @@ export class LoginPage implements OnInit {
   ngOnInit() {
 
   }
-  8619
-  login() {
+  async login() {
     const checkLogin = document.getElementById('checkLogin');
     axios.post('http://3.139.244.188:3000/login', {
       username: this.username,
@@ -45,87 +44,95 @@ export class LoginPage implements OnInit {
         localStorage.username = res.data[0].username;
         localStorage.userID = res.data[0].u_id;
         console.log(localStorage.userID);
+        this.getSetting();
         location.href = ('/home');
       })
-    this.initSetting();
+
+  }
+
+  async getSetting() {
+    axios.get('http://3.139.244.188:3000/currentFont', {
+      params: {
+        userID: localStorage.userID
+      }
+    })
+      .then(res => {
+        console.log("받아온 font id 값 : " + res.data[0].font_id);
+        localStorage.fontId = res.data[0].font_id;
+      });
+
+    axios.get('http://3.139.244.188:3000/currentTheme', {
+      params: {
+        userID: localStorage.userID
+      }
+    })
+      .then(res => {
+        localStorage.themeId = res.data[0].theme_id;
+        console.log("theme id 값 : " + localStorage.themeId);
+      });
+
+    await this.initSetting();
   }
 
   initSetting() {
 
     const change = document.querySelector('body');
 
-    axios.get('http://3.139.244.188:3000/currentTheme',{
-      params:{
-        userID: localStorage.userID
-      }
-    })
-      .then(async res => {
-        localStorage.themeId = res.data[0].theme_id;
-        if (localStorage.themeId == null) { // default
-          console.log("기본!")
-          localStorage.t_primary = '#FFF3A9';
-          localStorage.t_check = '#97C1E87E';
-          localStorage.t_complete = '#FDE2E2';
-          localStorage.t_button = '#F9D667';
-          localStorage.t_darkYellow = '#FDE2E2';
-          localStorage.t_background = '#FCECB6';
-        }
+    if (localStorage.themeId == null || localStorage.themeId == 1) { // default
+      console.log("기본!")
+      localStorage.t_primary = '#FFF3A9';
+      localStorage.t_check = '#97C1E87E';
+      localStorage.t_complete = '#FDE2E2';
+      localStorage.t_button = '#F9D667';
+      localStorage.t_darkYellow = '#FDE2E2';
+      localStorage.t_background = '#FCECB6';
+    }
 
-        if (localStorage.themeId == 2) {
-          console.log("분홍 먼지!")
-          localStorage.t_primary = '#FFD6D6';
-          localStorage.t_check = '#FFF0F0';
-          localStorage.t_complete = '#F1A0A0';
-          localStorage.t_button = '#F1B0B0';
-          localStorage.t_darkYellow = '#F2D0D0';
-          localStorage.t_background = '#FFD6D6';
-        }
+    if (localStorage.themeId == 2) {
+      console.log("분홍 먼지!")
+      localStorage.t_primary = '#FFD6D6';
+      localStorage.t_check = '#FFF0F0';
+      localStorage.t_complete = '#F1A0A0';
+      localStorage.t_button = '#F1B0B0';
+      localStorage.t_darkYellow = '#F2D0D0';
+      localStorage.t_background = '#FFD6D6';
+    }
 
-        if (localStorage.themeId == 3) {
-          console.log("파랑 먼지!")
-          localStorage.t_primary = '#DBE8F3';
-          localStorage.t_check = '#E4EFFA';
-          localStorage.t_complete = '#97C1E8';
-          localStorage.t_button = '#ACCBE5';
-          localStorage.t_darkYellow = '#BCD9F4';
-          localStorage.t_background = '#DBE8F3';
-        }
+    if (localStorage.themeId == 3) {
+      console.log("파랑 먼지!")
+      localStorage.t_primary = '#DBE8F3';
+      localStorage.t_check = '#E4EFFA';
+      localStorage.t_complete = '#97C1E8';
+      localStorage.t_button = '#ACCBE5';
+      localStorage.t_darkYellow = '#BCD9F4';
+      localStorage.t_background = '#DBE8F3';
+    }
 
-        if (localStorage.themeId == 4) {
-          console.log("초록 먼지!")
-          localStorage.t_primary = '#B3D9A1';
-          localStorage.t_check = '#F5FCF0';
-          localStorage.t_complete = '#B3D9A1';
-          localStorage.t_button = '#90D371';
-          localStorage.t_darkYellow = '#D2F0C4';
-          localStorage.t_background = '#CEEDC0';
-        }
-      });
+    if (localStorage.themeId == 4) {
+      console.log("초록 먼지!")
+      localStorage.t_primary = '#B3D9A1';
+      localStorage.t_check = '#F5FCF0';
+      localStorage.t_complete = '#B3D9A1';
+      localStorage.t_button = '#90D371';
+      localStorage.t_darkYellow = '#D2F0C4';
+      localStorage.t_background = '#CEEDC0';
+    }
 
-    axios.get('http://3.139.244.188:3000/currentFont',{
-      params:{
-        userID: localStorage.userID
-      }
-    })
-      .then(async res => {
-        console.log("받아온 font id 값 : " + res.data[0].font_id);
-        localStorage.fontId = res.data[0].font_id;
-        if (localStorage.fontId == 1) {
-          localStorage.f_family = 'NexonGothic';
-        }
-        if (localStorage.fontId == 2) {
-          localStorage.f_family = 'PrettyNight';
-        }
-        if (localStorage.fontId == 3) {
-          localStorage.f_family = 'DoHyeon';
-        }
-        if (localStorage.fontId == 4) {
-          localStorage.f_family = 'PoorStory';
-        }
-        if (localStorage.fontId == 5) {
-          localStorage.f_family = 'NexonFootball';
-        }
-      });
+    if (localStorage.fontId == 1) {
+      localStorage.f_family = 'NexonGothic';
+    }
+    if (localStorage.fontId == 2) {
+      localStorage.f_family = 'PrettyNight';
+    }
+    if (localStorage.fontId == 3) {
+      localStorage.f_family = 'DoHyeon';
+    }
+    if (localStorage.fontId == 4) {
+      localStorage.f_family = 'PoorStory';
+    }
+    if (localStorage.fontId == 5) {
+      localStorage.f_family = 'NexonFootball';
+    }
 
     change.style.setProperty('--ion-font-family', localStorage.f_family);
 
