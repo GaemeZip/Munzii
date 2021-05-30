cdbb9c40991094e784686ada22b3fe4504c2a0f0
Jihyoung <59171154+HelloJihyoung@users.noreply.github.com>
2021-05-30 19:24:40 +0900

    modified :: setting

--- server/router/settings.router.js
+++ server/router/settings.router.js
@@ -4,7 +4,27 @@ const settingsController = require('../controllers/settings.controller');
 const router = express.Router();
 
 router
-.route('/font')
+.route('/font/readFont')
 .get(settingsController.readFont)
+
+router
+.route('/font/currentFont')
+.get(settingsController.currentFont)
+
+router
+.route('/theme/readTheme')
+.get(settingsController.readTheme)
+
+router
+.route('/theme/currentTheme')
+.get(settingsController.currentTheme)
+
+router
+.route('/startDay/readStartDay')
+.get(settingsController.readTheme)
+
+router
+.route('/startDay/currentStartDay')
+.get(settingsController.currentTheme)
 
 module.exports = router;
